6b620b80163bede53d5ba605047c72a98291cce9
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-11-26 19:43:13 +0900

    ss

--- YOLO/src/main/java/yolo/vo/UserVO.java
+++ YOLO/src/main/java/yolo/vo/UserVO.java
@@ -15,7 +15,7 @@ public class UserVO {
 	private LocalDateTime wdate;
 	private LocalDateTime udate;
 	
-	// insert 시 사용할 생성자 테스트
+	// insert 시 사용할 생성자 테스트222
 	public UserVO(String profileImage, String nickName, String email, String password, int p_qId, String p_answer) {
 		this.profileImage = profileImage;
 		this.nickName = nickName;
