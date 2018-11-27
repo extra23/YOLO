bf58e2025d56a43ac349e3f7ce23ec13eb52b287
extra23 <KOITT@KOITT-PC>
2018-11-27 19:00:29 +0900

    userSErvice

---
+++ YOLO/src/main/java/yolo/service/UserService.java
@@ -0,0 +1,34 @@
+package yolo.service;
+
+import java.util.List;
+
+import yolo.vo.UserVO;
+
+public class UserService implements InterfaceUserService {
+
+	public void addUser(UserVO user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public UserVO readUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<UserVO> readUserList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void modifyUser(UserVO user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void removeUser(int user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
