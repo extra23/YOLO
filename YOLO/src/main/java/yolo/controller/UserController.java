80b27156ee3194382a7169ba9de4ae8e65f4d26e
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-11-27 20:46:42 +0900

    UserController -회원가입 -회원페이지 보기 -회원 정보 수정 -회원 탈퇴

--- YOLO/src/main/java/yolo/controller/UserController.java
+++ YOLO/src/main/java/yolo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,6 +19,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	//리스트로 유저의 정보 받아오는 것
 	@RequestMapping("/getUserList.do")
 	public String readUserList(Model model) {
 		List<UserVO> userList = userService.readUserList();
@@ -26,11 +28,13 @@ public class UserController {
 		return "userList";
 	}
 	
+	//회원가입 폼으로 이동
 	@RequestMapping("/addUserForm.do")
 	public  ModelAndView addUserForm() {
-		return new ModelAndView("login");
+		return new ModelAndView("join");
 	}
 	
+	//회원가입 후 메인으로 이동
 	@RequestMapping("/addUser.do")
 	public String addUser(
 			@RequestParam("profileImage") String profileImage,
@@ -42,7 +46,38 @@ public class UserController {
 			) {
 		UserVO user = new UserVO(profileImage, nickName, email, password, p_qId, p_answer);
 		userService.addUser(user);
-		return "redirect:/mainBoard";
+		return "mainBoard";
+	}
+	
+	//프로필 페이지 띄울 때
+	@RequestMapping("getUser.do")
+	public String readUser(Model model, @RequestParam("email") String email){
+		model.addAttribute("user",userService.readUSerByEmail(email));
+		return "userView";
+	}
+	
+	//수정 페이지 열기
+	@RequestMapping(value="/modifyUserForm.do",method=RequestMethod.GET)
+	public ModelAndView modifyUserForm(@RequestParam("userId") int userId, ModelAndView mv) {
+		UserVO user = userService.readUserByUserId(userId);
+		mv.addObject("user",user);
+		mv.setViewName("userModify");
+		return mv;
+	}
+	//수정해서 프로필 페이지로 넘기기
+	@RequestMapping(value = "/modify.do", method=RequestMethod.POST)
+	public String modifyUser(String profileImage, String nickName, String password, int p_qId, String p_answer) {
+		UserVO user = new UserVO(profileImage, nickName, password,p_qId,p_answer);
+		userService.modifyUser(user);
+		return "userView";
+	}
+	
+	//유저 탈퇴하고 메인 페이지로 복귀하기
+	@RequestMapping(value = "/remove.do/{userId}")
+	public String removeUser(@RequestParam("userId") int userId) {
+		userService.removeUser(userId);
+		
+		return "mainBoard";
 	}
 	
 	
