83091d7274657716fb3331dad9b285e7f5f2dc59
sohyun <sohyun@LAPTOP-8CPNIE4K>
2018-11-27 21:46:30 +0900

    로그인이니이니이니 으가가가가가가가

--- YOLO/src/main/java/yolo/controller/LoginoutController.java
+++ YOLO/src/main/java/yolo/controller/LoginoutController.java
@@ -1,44 +1,62 @@
 package yolo.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import yolo.dao.UserDAO;
+import yolo.exception.LoginFailException;
+import yolo.service.LoginService;
 import yolo.vo.UserVO;
 
 @Controller
 public class LoginoutController {
 	
+	// 무결성 체크11
+	@Autowired
+	LoginService loginService;
+	
 	// 이미 빈으로 등록된 userDAO를 자동 매칭해서 주입함.
 	@Autowired
 	private UserDAO userDAO;
-	
-	// 로그인화면
-	@RequestMapping("login.do")
-	public String login() {
-		// webapp/test/login.jsp로 이동
-		return "webapp/test/login";
-	}
-	
-	// 로그인 처리
-	@RequestMapping("loginCheck.do")
-	public ModelAndView loginCheck(String email, String password) {
-		ModelAndView mav = new ModelAndView();
-		// userVO를 이용해서 email객체를 select 해옴
-		UserVO user = userDAO.selectUserList(email);
-		
-		// email 객체가 존재하지 않을 때의 처리
-		if(email == null) {
-			throw new LoginFailException("존재하지 않는 사용자");
-			}
-		
-		// email 객체가 존재하지만 객체의 비밀번호와 입력한 비밀번호가 일치하지 않을 때
-		if(user.setPassword(password) != password) {
-			throw new LoginFailException("비밀번호가 일치하지 않음.");
-			}
-		
-	}
 
+	// 무결성 체크(모르겟다)
+	public String processSubmit(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+			// 1. request로 parameter를 받음(eamil, password)
+			String eamil = req.getParameter("email");
+			String password = req.getParameter("password");
+			
+			// 2. 받아온 paramter의 무결성 체크(비었는지 안비었는지)
+			Map<String, Boolean> errors = new HashMap<String, Boolean>();
+			if(email == null || email.isEmpty()) {
+				errors.put("email", true);
+			}
+			if(password == null || password.isEmpty()) {
+				errors.put("password", true);
+			}
+			req.setAttribute("errors", errors);
+			if(!errors.isEmpty()) {
+				return FORM_VIEW;
+			}
+			
+			// 3. 무결성 체크 후 비지 않았다면 객체 받고 객체의 메소드를 통해서 인증
+			String login = ModelAndView loginCheck
+			
+		}
+
+	/*
+	 * public boolean preHalder(HttpServletRequest request, HttpServletResponse
+	 * response, Object handler)throw Exception{ HttpSession session =
+	 * request.getSession(false); }
+	 */
 }
