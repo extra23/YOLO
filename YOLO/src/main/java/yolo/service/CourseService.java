413fcc6af9da748ed48e4987d1fe4a455f22480a
sohyun <sohyun@LAPTOP-8CPNIE4K>
2018-12-01 11:54:13 +0900

    짜장면 먹고싶다

---
+++ YOLO/src/main/java/yolo/service/CourseService.java
@@ -0,0 +1,44 @@
+package yolo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import yolo.dao.InterfaceCourseDAO;
+import yolo.vo.CourseVO;
+import yolo.vo.SearchVO;
+
+public class CourseService implements InterfaceCourseService {
+
+	@Autowired
+	InterfaceCourseDAO courseDAO;
+	
+	public void addCourse(CourseVO course) {
+		courseDAO.insertCourse(course);
+	}
+	
+	public CourseVO readCourseByUserId(int userId) {
+		return courseDAO.selectCourseByUserId(userId);
+	}
+	
+	public CourseVO readCourseByCourseId(int courseId) {
+		return courseDAO.selectCourseByCourseId(courseId);
+	}
+	
+	public List<CourseVO> readCourseList(){
+		return courseDAO.selectCourseList();
+	}
+	
+	public void modifyCourse(CourseVO course) {
+		courseDAO.updateCourse(course);
+	}
+	
+	public void removeCourse(int courseId) {
+		courseDAO.deleteCourse(courseId);
+	}
+	
+	public List<SearchVO> searchCourse(String cTitle){
+		return courseDAO.searchCourse(cTitle);
+	}
+
+}
