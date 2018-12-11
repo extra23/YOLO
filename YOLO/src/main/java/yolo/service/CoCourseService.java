966f8ff98aaedecf062886aead26f49b3f25ee1d
KOITT-02-A <KOITT-02-A@KOITT-02-A-PC>
2018-12-11 19:37:24 +0900

    cocourseservice

--- YOLO/src/main/java/yolo/service/CoCourseService.java
+++ YOLO/src/main/java/yolo/service/CoCourseService.java
@@ -3,10 +3,12 @@ package yolo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import yolo.dao.InterfaceCoCourseDAO;
 import yolo.vo.CoCourseAndCourseVO;
 
+@Service("CoCourseService")
 public class CoCourseService implements InterfaceCoCourseService {
 
 	@Autowired
