f31684476b68dcb0d184b11d2d49734ece5cc7e8
KOITT-02-A <KOITT-02-A@KOITT-02-A-PC>
2018-12-19 21:20:02 +0900

    costudy 관련 service

--- YOLO/src/main/java/yolo/service/CoCourseService.java
+++ YOLO/src/main/java/yolo/service/CoCourseService.java
@@ -1,5 +1,6 @@
 package yolo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import yolo.dao.InterfaceCoCourseDAO;
 import yolo.vo.CoCourseAndCourseVO;
 import yolo.vo.CoCourseListVO;
+import yolo.vo.CoCourseListViewVO;
+import yolo.vo.MoLink;
 
 @Service("CoCourseService")
 public class CoCourseService implements InterfaceCoCourseService {
@@ -32,9 +35,43 @@ public class CoCourseService implements InterfaceCoCourseService {
 		coCourseDAO.deleteCoCourse(costudy_courseId);
 	}
 	
-	public List<CoCourseListVO> readcoCourseList(int userId) {
-		// TODO Auto-generated method stub
-		return coCourseDAO.coCourseList(userId);
+	public List<CoCourseListViewVO> readcoCourseList(int userId) {
+		
+		List<CoCourseListVO> cocList = coCourseDAO.coCourseList(userId);
+		List<CoCourseListViewVO> coCourseListViewVOs = new ArrayList<CoCourseListViewVO>();
+		List<MoLink> mLinks = new ArrayList<MoLink>();
+		
+		
+    for (int i = 0; i< cocList.size(); i++) {//디비에있던 로우수만큼 반복해서 도는 for문 생성.
+			
+			//리스트에선 1, 1+1 번째의 모듈 아이디가 서로 같으면!!
+			if(i+1 < cocList.size() && cocList.get(i).getCourseId() == cocList.get(i+1).getCourseId()) {
+				mLinks.add(new MoLink(cocList.get(i).getmTitle(),cocList.get(i).getModuleId()));
+				continue;
+			}else {
+				mLinks.add(new MoLink(cocList.get(i).getmTitle(),cocList.get(i).getModuleId()));
+			}
+			
+
+			coCourseListViewVOs.add(
+					new CoCourseListViewVO(
+							cocList.get(i).getCostudy_courseId(),
+							cocList.get(i).getCourseId(),
+							cocList.get(i).getUserId(),
+							mLinks,
+							cocList.get(i).getcTitle(),
+							cocList.get(i).getNickname(),
+							cocList.get(i).getUserId(),
+							cocList.get(i).getUdate()
+							)
+					);
+		
+			mLinks=new ArrayList<MoLink>();
+		}
+		return coCourseListViewVOs;
 	}
+
+	
+
 
 }
--- YOLO/src/main/java/yolo/service/CoModuleService.java
+++ YOLO/src/main/java/yolo/service/CoModuleService.java
@@ -1,13 +1,16 @@
 package yolo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import yolo.dao.InterfaceCoModuleDAO;
+import yolo.vo.CoLink;
 import yolo.vo.CoModuleAndModuleVO;
 import yolo.vo.CoModuleListVO;
+import yolo.vo.CoModuleListViewVO;
 
 @Service("CoModuleService")
 public class CoModuleService implements InterfaceCoModuleService {
@@ -23,9 +26,7 @@ public class CoModuleService implements InterfaceCoModuleService {
 		return coModuleDAO.selectJoinModule(userId);
 	}
 
-	public List<CoModuleListVO> readcoModuleList(int userId) {
-		return coModuleDAO.coModuleList(userId);
-	}
+
 
 	public CoModuleAndModuleVO readCoModuleAndModuleByModuleIdAndUserId(CoModuleAndModuleVO coModuleAndModule) {
 		return coModuleDAO.selectCoModuleAndModuleByModuleIdAndUserId(coModuleAndModule);
@@ -34,5 +35,41 @@ public class CoModuleService implements InterfaceCoModuleService {
 	public void removeCoModule(int costudy_moduleId) {
 		coModuleDAO.deleteCoModule(costudy_moduleId);
 	}
+	
+	public List<CoModuleListViewVO> readcoModuleList(int userId) {
+		List<CoModuleListVO> comList= coModuleDAO.coModuleList(userId);
+		List<CoModuleListViewVO> coModuleListViewVOs = new ArrayList<CoModuleListViewVO>();
+		List<CoLink> cLinks= new ArrayList<CoLink>();
+		
+		for (int i = 0; i< comList.size(); i++) {//디비에있던 로우수만큼 반복해서 도는 for문 생성.
+			
+			//리스트에선 1, 1+1 번째의 모듈 아이디가 서로 같으면!!
+			if(i+1 < comList.size() && comList.get(i).getModuleId() == comList.get(i+1).getModuleId()) {
+				cLinks.add(new CoLink(comList.get(i).getcTitle(),comList.get(i).getCourseId()));
+				continue;
+			}else {
+				cLinks.add(new CoLink(comList.get(i).getcTitle(),comList.get(i).getCourseId()));
+			}
+			
+			
+			System.out.println(comList.get(i).getCourseId()+": i"+i);
+			coModuleListViewVOs.add(
+					new CoModuleListViewVO(
+							comList.get(i).getCostudy_moduleId(),
+							comList.get(i).getModuleId(),
+							comList.get(i).getUserId(),
+							cLinks,
+							comList.get(i).getmTitle(),
+							comList.get(i).getNickname(),
+							comList.get(i).getUserId(),
+							comList.get(i).getUdate()
+							)
+					);
+			cLinks= new ArrayList<CoLink>();
+			
+		}
+		return coModuleListViewVOs;
+	}
+
 
 }
